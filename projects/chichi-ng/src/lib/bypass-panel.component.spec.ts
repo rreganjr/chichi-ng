e3d62baae5682b37ff9be45c3b2320d8183a6de1
Ron Regan <rregan@platformq.com>
2019-05-07 19:55:08 -0400

    adding tests

--- projects/chichi-ng/src/lib/bypass-panel.component.spec.ts
+++ projects/chichi-ng/src/lib/bypass-panel.component.spec.ts
@@ -1,10 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser'
 
 import { ChiChiBypassPanelComponent } from './bypass-panel.component';
 
 describe('BypassPanelComponent', () => {
   let component: ChiChiBypassPanelComponent;
   let fixture: ComponentFixture<ChiChiBypassPanelComponent>;
+  let debugElement : DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,29 @@ describe('BypassPanelComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ChiChiBypassPanelComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start with left panel active', () => {
+    expect(component.rightPanelActive == false);
+  });
+
+  it('should have a container div', () => {
+    expect(debugElement.query(By.css('div.container')).nativeElement.innerText).toBe('');
+  });
+
+  it('should have a left-panel in container div', () => {
+    expect(debugElement.query(By.css('div.container > div.left-panel')).nativeElement.innerText).toBe('');
+  });
+
+  it('should have a right-panel in container div', () => {
+    expect(debugElement.query(By.css('div.container > div.right-panel')).nativeElement.innerText).toBe('');
+  });
+
 });
