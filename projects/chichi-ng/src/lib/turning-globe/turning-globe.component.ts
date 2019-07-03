a7ca2ac500fae0eae17bb5da7b96cd1b34278403
rreganjr <rreganjr@acm.org>
2019-07-03 00:58:39 -0400

    changed size to 360, at 720 the bottom half was cut off. adjusted shadow styling, added withShadow attribute so it is possible to remove the shadow.

--- projects/chichi-ng/src/lib/turning-globe/turning-globe.component.ts
+++ projects/chichi-ng/src/lib/turning-globe/turning-globe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, HostBinding, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 export const spinningGlobeAnimation = trigger('spin', [
@@ -6,11 +6,11 @@ export const spinningGlobeAnimation = trigger('spin', [
   state('out', style({})),
   transition('in => out', animate('{{ time }}', keyframes([
     style({ backgroundPosition: '0 0', offset: 0.0 }),
-    style({ backgroundPosition: '720px 0', offset: 1.0 })
+    style({ backgroundPosition: '360px 0', offset: 1.0 })
   ]))),
   transition('out => in', animate('{{ time }}', keyframes([
     style({ backgroundPosition: '0 0', offset: 0.0 }),
-    style({ backgroundPosition: '720px 0', offset: 1.0 })
+    style({ backgroundPosition: '360px 0', offset: 1.0 })
   ])))
 ]);
 
@@ -28,6 +28,7 @@ export class ChiChiTurningGlobeComponent implements OnInit, AfterViewInit  {
   @Input() globeImage: string;
   @Input() height: string;
   @Input() width: string;
+  @Input() withShadow: boolean = false;
   @Input() secondsPerRotation: string = '5s';
 
   constructor() { }
