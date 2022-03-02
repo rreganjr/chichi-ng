0550f55eece0acbffca2123ea9bebe40740ccf6d
rreganjr <rreganjr@acm.org>
2022-03-02 04:03:32 +0000

    improvements to tick lines, I added a primary for labeled ticks and between concept for unlabeled and exception for day markers, looking better except for 2 day view as hour and day label overlap

--- projects/chichi-ng/src/lib/visual-scheduler/timeline/timeline.component.ts
+++ projects/chichi-ng/src/lib/visual-scheduler/timeline/timeline.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, Input, OnDestroy, OnInit, Renderer2 } from '@angular/core';
-import { DateTime, DateTimeFormatOptions, Duration } from 'luxon';
+import { DateTime, DateTimeFormatOptions, DateTimeUnit, Duration } from 'luxon';
 import { Subscription } from 'rxjs';
 import { Timescale } from '../timescale.model';
 import { VisualSchedulerService } from '../visual-scheduler.service';
@@ -13,10 +13,10 @@ export class TimelineComponent implements OnInit, OnDestroy {
 
   private _timescaleSubscription?: Subscription;
   private _timescale!: Timescale;
-
-  @Input() showLabels: boolean = false;
   private _dateFormatOptions: DateTimeFormatOptions | undefined = {dateStyle: 'short'};
   private _timeFormatOptions: DateTimeFormatOptions | undefined = {timeStyle: 'short'};
+
+  @Input() showLabels: boolean = false;
  
   constructor(
     private visualSchedulerService: VisualSchedulerService,
@@ -43,16 +43,14 @@ export class TimelineComponent implements OnInit, OnDestroy {
    * @returns the percent width of each time division in the timeline for the current timescale.
    */
   private get timeDivisionWidth(): string {
-    return (this._timescale.visibleDuration.as('minutes') / this.timeDivisionDuration.as('minutes')) * 100  + '%';
+    return (this._timescale.visibleDuration.as('minutes') / this.betweenTicksDuration.as('minutes')) * 100  + '%';
   }
 
-  private get timeDivisionDuration(): Duration {
+  private get primaryTicksDuration(): Duration {
     const visibleHours = this._timescale.visibleDuration.hours;
     if (visibleHours < 24) {
-      return Duration.fromDurationLike({minutes: 15});
+      return Duration.fromDurationLike({hours: 1});
     } else if (visibleHours < 2*24) {
-      return Duration.fromDurationLike({minutes: 30});
-    } else if (visibleHours < 3*24) {
       return Duration.fromDurationLike({hours: 2});
     } else if (visibleHours < 5*24) {
       return Duration.fromDurationLike({hours: 6});
@@ -60,6 +58,30 @@ export class TimelineComponent implements OnInit, OnDestroy {
       return Duration.fromDurationLike({hours: 12});
     } else {
       return Duration.fromDurationLike({days: 1});
+    }
+  }
+
+  private get betweenTicksDuration(): Duration {
+    const visibleHours = this._timescale.visibleDuration.hours;
+    if (visibleHours < 12) {
+      return Duration.fromDurationLike({minutes: 5});
+    } else if (visibleHours < 24) {
+      return Duration.fromDurationLike({minutes: 15});
+    } else if (visibleHours < 3*24) {
+      return Duration.fromDurationLike({minutes: 30});
+    } else if (visibleHours < 5*24) {
+      return Duration.fromDurationLike({hours: 3});
+    } else {
+      return Duration.fromDurationLike({hours: 6});
+    }
+  }
+
+  private get startAt(): DateTimeUnit {
+    const visibleHours = this._timescale.visibleDuration.hours;
+    if (visibleHours < 3*24) {
+      return 'hour';
+    } else {
+      return 'day';
     }
   }
 
@@ -72,25 +94,37 @@ export class TimelineComponent implements OnInit, OnDestroy {
         this.renderer.removeChild(this.timelineElement.nativeElement, child);
       }
 
-      const startTick: DateTime = this._timescale.boundsInterval.start.startOf('hour').plus(this._timescale.offsetDuration);
+      const startTick: DateTime = this._timescale.boundsInterval.start.startOf(this.startAt).plus(this._timescale.offsetDuration);
       const lastTick: DateTime = startTick.plus(this._timescale.visibleDuration);
-      const tickDuration: Duration = this.timeDivisionDuration;
+      const primaryTicksDuration: Duration = this.primaryTicksDuration;
+      const betweenTicksDuration: Duration = this.betweenTicksDuration;
 
-      // drawing the time lines
-      for (let tick: DateTime = startTick; tick <= lastTick; tick = tick.plus(tickDuration) ) {
-        this.renderer.appendChild(this.timelineElement.nativeElement, (tick.minute === 0 ? this.makeHourElement(tick) : this.makeSubHourElement(tick)));
+      // drawing the tick marks
+      for (let primaryTick: DateTime = startTick; primaryTick <= lastTick; primaryTick = primaryTick.plus(primaryTicksDuration)) {
+        this.renderer.appendChild(this.timelineElement.nativeElement, this.makePrimaryTickElement(primaryTick));
+        const nextPrimaryTick:DateTime = primaryTick.plus(primaryTicksDuration);
+        for (let betweenTick: DateTime = primaryTick.plus(betweenTicksDuration); betweenTick < nextPrimaryTick; betweenTick = betweenTick.plus(betweenTicksDuration)) {
+          // draw the inbetween marks, if one of these falls on the start of a day, add a primary mark instead
+          if (betweenTick.hour === 0 && betweenTick.minute === 0) {
+            this.renderer.appendChild(this.timelineElement.nativeElement, this.makePrimaryTickElement(betweenTick));
+          } else {
+            this.renderer.appendChild(this.timelineElement.nativeElement, this.makeBetweenTickElement(betweenTick));
+          }
+        }
       }
     }
   }
 
-  private makeSubHourElement(dateTime: DateTime): HTMLDivElement {
+  private makeBetweenTickElement(dateTime: DateTime): HTMLDivElement {
+    console.log(`makeBetweenTickElement: ${dateTime}`);
     const element: HTMLDivElement = this.renderer.createElement('div');
     element.style.width = this.timeDivisionWidth;
     element.className = 'subHour';
     return element;
   }
 
-  private makeHourElement(dateTime: DateTime): HTMLDivElement {
+  private makePrimaryTickElement(dateTime: DateTime): HTMLDivElement {
+    console.log(`makePrimaryTickElement: ${dateTime}`);
     const element: HTMLDivElement = this.renderer.createElement('div');
     element.style.width = this.timeDivisionWidth;
     element.className = (dateTime.hour === 0) ? 'day' : 'hour';
