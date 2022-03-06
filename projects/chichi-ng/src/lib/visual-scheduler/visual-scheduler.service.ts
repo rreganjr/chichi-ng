67c1ca46a51b6a5a09b77e9e7b05648660221543
Ron Regan <rregan@platformq.com>
2022-03-06 02:48:01 +0000

    adding AgendaItem concept, explicit resource name, passing resource name to channel level

--- projects/chichi-ng/src/lib/visual-scheduler/visual-scheduler.service.ts
+++ projects/chichi-ng/src/lib/visual-scheduler/visual-scheduler.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { Observable, ReplaySubject } from 'rxjs';
 import { Duration, Interval } from 'luxon';
 import { Timescale } from './timescale.model';
+import { AgendaItem, AgendaItemLabeler } from './resource/agenda-box/agenda-item.model';
+
+type ResourceChannelMapKey = {resourceName: string, channelName: string};
+type ResourceChannelMapValue = {agendaItems: AgendaItem[], replaySubject: ReplaySubject<AgendaItem[]>};
 
 @Injectable({
   providedIn: null
@@ -11,24 +15,27 @@ export class VisualSchedulerService {
   private _timescale = new Timescale(
     Interval.fromDateTimes(new Date(), new Date(new Date().getTime() + 24 * 60 * 60 * 1000)),
   );
+  private _timescaleSubject: ReplaySubject<Timescale> = new ReplaySubject(1);
 
-  private timescaleSubject: ReplaySubject<Timescale> = new ReplaySubject(1);
+  private _agendaItems: AgendaItem[] = [];
+  private _agendaItemsSubject: ReplaySubject<AgendaItem[]> = new ReplaySubject(1);
+  private _agendaItemsByResourceChannelMap: Map<ResourceChannelMapKey,ResourceChannelMapValue> = new Map<ResourceChannelMapKey, ResourceChannelMapValue>();
 
   constructor() {
-    this.timescaleSubject.next(this._timescale);
+    this._timescaleSubject.next(this._timescale);
   }
 
   public getTimescale$(): Observable<Timescale> {
-    return this.timescaleSubject.asObservable();
+    return this._timescaleSubject.asObservable();
   }
 
   /**
    * The min and max date/time of schedules
    * @param interval the start date and time to the end date and time
    */
-  public setBoundsInterval(interval: Interval) {
+  public setBoundsInterval(interval: Interval): void {
     this._timescale = new Timescale(interval, this._timescale.visibleDuration, this._timescale.offsetDuration);
-    this.timescaleSubject.next(this._timescale);
+    this._timescaleSubject.next(this._timescale);
   }
 
   /**
@@ -37,7 +44,7 @@ export class VisualSchedulerService {
    * 
    * @param offsetHours - the hours from the bounds start time to the start of the visible hours
    */
-  public setTimeScaleOffsetHours(offsetHours: number) {
+  public setTimeScaleOffsetHours(offsetHours: number): void {
     console.log(`setTimeScaleOffsetHours(offsetHours = ${offsetHours})`);
     if (offsetHours >= 0) {
       offsetHours = Math.round(offsetHours);
@@ -48,7 +55,7 @@ export class VisualSchedulerService {
         newDuration = Duration.fromDurationLike({hours: offsetHours});
       }
       this._timescale = new Timescale(this._timescale.boundsInterval, this._timescale.visibleDuration, newDuration);
-      this.timescaleSubject.next(this._timescale);  
+      this._timescaleSubject.next(this._timescale);  
     }
   }
 
@@ -56,7 +63,7 @@ export class VisualSchedulerService {
    * Set the viewport of the schedule to the specificed number of hours
    * @param visibleHours - The size of what is visible in hours, minimum = 1 hour maximum = 7 days
    */
-  public setTimeScaleVisibleHours(visibleHours: number) {
+  public setTimeScaleVisibleHours(visibleHours: number): void {
     visibleHours = Math.round(visibleHours);
     if (visibleHours < 3) {
       visibleHours = 3;
@@ -65,7 +72,43 @@ export class VisualSchedulerService {
     }
     if (visibleHours > 0 && visibleHours <= 7 * 24) {
       this._timescale = new Timescale(this._timescale.boundsInterval, Duration.fromDurationLike({hours: visibleHours}), this._timescale.offsetDuration);
-      this.timescaleSubject.next(this._timescale);
+      this._timescaleSubject.next(this._timescale);
     }
+  }
+
+  /**
+   * 
+   * @returns All the {@link AgendaItem}s in the {@link VisualSchedulerComponent}
+   */
+  public getAgendaItems$(): Observable<AgendaItem[]> {
+    return this._agendaItemsSubject.asObservable();
+  }
+
+  /**
+   * 
+   * @param resourceName The name of a specific {@link ResourceComponent}
+   * @param channelName The name of a {@link ChannelComponent} in the named resource
+   * @returns The {@link AgendaItem}s in that channel of the resource
+   */
+  public getAgendaItemsByResourceChannel$(resourceName: string, channelName: string): Observable<AgendaItem[]> {
+    const key = {resourceName: resourceName, channelName: channelName};
+    if (this._agendaItemsByResourceChannelMap.get(key) == undefined) {
+      this._agendaItemsByResourceChannelMap.set(key, {agendaItems: [], replaySubject: new ReplaySubject<AgendaItem[]>(1)});
+    }
+    const mapValue = this._agendaItemsByResourceChannelMap.get(key);
+    return mapValue!.replaySubject.asObservable();
+  }
+
+  public addAgendaItem(resourceName: string, channelName: string, bounds: Interval, data: object, labeler: AgendaItemLabeler): void {
+    const agendaItem = new AgendaItem(resourceName, channelName, bounds, data, labeler)
+    this._agendaItems.push(agendaItem);
+    const key = {resourceName: resourceName, channelName: channelName};
+    if (this._agendaItemsByResourceChannelMap.get(key) == undefined) {
+      this._agendaItemsByResourceChannelMap.set(key, {agendaItems: [], replaySubject: new ReplaySubject<AgendaItem[]>(1)});
+    }
+    const mapValue = this._agendaItemsByResourceChannelMap.get(key);
+    mapValue!.agendaItems.push(agendaItem);
+    mapValue!.replaySubject.next(mapValue!.agendaItems);
+    this._agendaItemsSubject.next(this._agendaItems);
   }
 }
