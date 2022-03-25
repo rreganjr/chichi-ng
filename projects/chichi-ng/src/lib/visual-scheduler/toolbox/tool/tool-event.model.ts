ce2a078d9ad8d88fae40b407db1149f307c2c169
Ron Regan <rregan@platformq.com>
2022-03-25 00:43:16 +0000

    I added an id to ToolEvent and see that the same event is logged over and over, double hmmm

--- projects/chichi-ng/src/lib/visual-scheduler/toolbox/tool/tool-event.model.ts
+++ projects/chichi-ng/src/lib/visual-scheduler/toolbox/tool/tool-event.model.ts
@@ -1,15 +1,20 @@
 import { AgendaItem } from "../../resource/agenda-box/agenda-item.model";
 
 export class ToolEvent {
+    private static nextId: number = 1;
     public static readonly CLEAR = new ToolEvent('CLEAR');
-    
+
+    public readonly id: number;
+
     constructor(
         public readonly action: 'CLEAR'|'START'|'END'|'DROP'|'EDIT',
         public readonly toolType: string = '',
         public readonly event: DragEvent|Event|null = null,
         public readonly agendaItem: AgendaItem|null = null
-    ) {}
-
+    ) {
+        this.id = ToolEvent.nextId++;
+    }
+    
     public isStart(): boolean {
         return this.action === 'START';
     }
