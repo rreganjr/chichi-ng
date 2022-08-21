f2dd29105f1f50098a630a766a7652b43f8da15e
rreganjr <rreganjr@acm.org>
2022-08-21 18:02:34 +0000

    add tests for out of bounds regions

--- projects/chichi-ng/src/lib/visual-scheduler/resource/agenda-box/timeline/timeline.component.spec.ts
+++ projects/chichi-ng/src/lib/visual-scheduler/resource/agenda-box/timeline/timeline.component.spec.ts
@@ -53,7 +53,6 @@ describe('TimelineComponent', () => {
     */
 
   it('TimelineComponent for viewport duration of one hour should have primaryTicks and betweenTicks  ', (done: DoneFn) => {
-    console.log(`hey Jude`);
     visualSchedulerService.getTimescale$().pipe(first()).subscribe(
       (timescale:Timescale) => {
         console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
@@ -71,7 +70,6 @@ describe('TimelineComponent', () => {
   })
 
   it('TimelineComponent for viewport duration of twelve hours should have primaryTicks and betweenTicks  ', (done: DoneFn) => {
-    console.log(`hey Jude`);
     visualSchedulerService.getTimescale$().pipe(first()).subscribe(
       (timescale:Timescale) => {
         console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
@@ -88,7 +86,6 @@ describe('TimelineComponent', () => {
   })
 
   it('TimelineComponent for viewport duration of one day should have primaryTicks and betweenTicks  ', (done: DoneFn) => {
-    console.log(`hey Jude`);
     visualSchedulerService.getTimescale$().pipe(first()).subscribe(
       (timescale:Timescale) => {
         console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
@@ -105,7 +102,6 @@ describe('TimelineComponent', () => {
   })
 
   it('TimelineComponent for viewport duration of two days should have primaryTicks and betweenTicks  ', (done: DoneFn) => {
-    console.log(`hey Jude`);
     visualSchedulerService.getTimescale$().pipe(first()).subscribe(
       (timescale:Timescale) => {
         console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
@@ -122,7 +118,6 @@ describe('TimelineComponent', () => {
   })
 
   it('TimelineComponent for viewport duration of three days should have primaryTicks and betweenTicks  ', (done: DoneFn) => {
-    console.log(`hey Jude`);
     visualSchedulerService.getTimescale$().pipe(first()).subscribe(
       (timescale:Timescale) => {
         console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
@@ -139,7 +134,6 @@ describe('TimelineComponent', () => {
   })
 
   it('TimelineComponent for viewport duration of five days should have primaryTicks and betweenTicks  ', (done: DoneFn) => {
-    console.log(`hey Jude`);
     visualSchedulerService.getTimescale$().pipe(first()).subscribe(
       (timescale:Timescale) => {
         console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
@@ -156,7 +150,6 @@ describe('TimelineComponent', () => {
   })
 
   it('TimelineComponent for viewport duration of seven days should have primaryTicks and betweenTicks  ', (done: DoneFn) => {
-    console.log(`hey Jude`);
     visualSchedulerService.getTimescale$().pipe(first()).subscribe(
       (timescale:Timescale) => {
         console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
@@ -173,7 +166,6 @@ describe('TimelineComponent', () => {
   })
 
   it('TimelineComponent for viewport duration of more than seven days should have primaryTicks and betweenTicks  ', (done: DoneFn) => {
-    console.log(`hey Jude`);
     visualSchedulerService.getTimescale$().pipe(first()).subscribe(
       (timescale:Timescale) => {
         console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
@@ -189,4 +181,39 @@ describe('TimelineComponent', () => {
     visualSchedulerService.setViewportDuration(Duration.fromDurationLike({days: 14}));
   })
 
+  it('TimelineComponent doesnt have outOfBounds regions when bounds match the primary ticks of hours', (done: DoneFn) => {
+    visualSchedulerService.getTimescale$().pipe(skip(1), first()).subscribe(
+      (timescale:Timescale) => {
+        console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
+        const timeLineInternals: object = component.getInternalConfig();
+        let counter: number = 0
+        fixture.debugElement.children.forEach( (debugElement: DebugElement) => {
+          console.log(`child[${counter}] = ${debugElement.name}.${debugElement.nativeElement.className} ${debugElement.nativeElement.id} `);
+          counter++;
+        })
+        done();
+      }
+    );
+    visualSchedulerService.setViewportDuration(Duration.fromDurationLike({hours: 6}));
+    visualSchedulerService.setBounds(new Date('2021-05-01 02:00:00'), new Date('2021-05-01 06:00:00'));
+
+  })
+
+  it('TimelineComponent doesnt have outOfBounds regions when bounds match the primary ticks of days', (done: DoneFn) => {
+    visualSchedulerService.getTimescale$().pipe(skip(1), first()).subscribe(
+      (timescale:Timescale) => {
+        console.log(`timescale seconds: ${timescale.visibleTimelineBounds.toDuration().as('seconds')}`, fixture.debugElement.children);
+        const timeLineInternals: object = component.getInternalConfig();
+        let counter: number = 0
+        fixture.debugElement.children.forEach( (debugElement: DebugElement) => {
+          console.log(`child[${counter}] = ${debugElement.name}.${debugElement.nativeElement.className} ${debugElement.nativeElement.id} `);
+          counter++;
+        })
+        done();
+      }
+    );
+    visualSchedulerService.setViewportDuration(Duration.fromDurationLike({hours: 6}));
+    visualSchedulerService.setBounds(new Date('2021-05-01 00:00:00'), new Date('2021-05-14 00:00:00'));
+
+  })
 });
